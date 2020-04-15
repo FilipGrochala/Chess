b3ed048af17ccb55ced29cb2178caaee2719028d
FilipGrochala <filip.grochala@o2.pl>
2020-04-15 17:52:48 +0200

    Podstawowe funkcje BoardManager

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -12,9 +12,25 @@ public class BoardManager : MonoBehaviour
 
     private void Update()
     {
+        UpdateSelection();
         DrawChessboard();
+        
     }
+    private void UpdateSelection()
+    {
+        if (!Camera.main)
+            return;
 
+        RaycastHit hit;
+        if (Physics.Raycast(
+            Camera.main.ScreenPointToRay(Input.mousePosition),
+            out hit,
+            25.0f,
+            LayerMask.GetMask("ChessPlane")))
+            {
+            Debug.Log(hit.point);
+            }
+    }
     private void DrawChessboard()
     {
         Vector3 width_line = Vector3.right * 8;
@@ -22,9 +38,12 @@ public class BoardManager : MonoBehaviour
 
         for(int i = 0; i<=8;i++)
         {
+            Vector3 start = Vector3.forward * i;
+            Debug.DrawLine(start, start + width_line);
             for (int j = 0; j <= 8; j++)
             {
-
+                start = Vector3.right * i;
+                Debug.DrawLine(start, start + height_line);
             }
         }
     }
