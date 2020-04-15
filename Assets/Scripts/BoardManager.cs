1d8961ac7b9ecb2b0482ad583259fa780c10fd6d
FilipGrochala <filip.grochala@o2.pl>
2020-04-15 17:11:49 +0200

    Podstawowe pliki:
    Dodanie podstawowych plików i utworzenie pierwszego skryptu

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    private const float tile_size = 1f; // rozmiar pola
+    private const float tile_offset = 0.5f; // margines pola
+
+    private int selectedX = -1; //wybrane pole
+    private int selectedY = -1;
+
+    private void Update()
+    {
+        DrawChessboard();
+    }
+
+    private void DrawChessboard()
+    {
+        Vector3 width_line = Vector3.right * 8;
+        Vector3 height_line = Vector3.forward * 8;
+
+        for(int i = 0; i<=8;i++)
+        {
+            for (int j = 0; j <= 8; j++)
+            {
+
+            }
+        }
+    }
+
+
+}
