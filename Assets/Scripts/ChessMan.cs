eae7a5c54608ae20b5aea334d57eba174d4685ae
FilipGrochala <filip.grochala@o2.pl>
2020-04-15 21:28:53 +0200

    Podstawy klasyfikacji figur
      Stworzono bazową abstrakcyjną klasę figury oraz klasy poszczególnych
      figur i przydzielomo je do prefabów

---
+++ Assets/Scripts/ChessMan.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ChessMan : MonoBehaviour
+{
+    public int CurrentX { get; set; }
+    public int CurrentY { get; set; }
+
+    public bool isWhite;
+
+}
